6c9047efb79e38abcab8856613c90c90036940b8
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-11-03 13:17:09 +0200

    Add haddock to isDirectoryContentsValid

--- src/System/Directory/Machine/Internal.hs
+++ src/System/Directory/Machine/Internal.hs
@@ -1,5 +1,6 @@
 module System.Directory.Machine.Internal where
 
+-- | Predicate to filter out relative paths: @"."@ and @".."@.
 isDirectoryContentsValid :: FilePath -> Bool
 isDirectoryContentsValid x = (x /= "." && x /= "..")
 
